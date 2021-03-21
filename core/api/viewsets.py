eba04e319b4ee765d457ff44770d6f5bdd527d3a
Luciano Celinski <luciano@gruposc.com.br>
2021-03-21 20:08:05 -0300

    Adiciona imagens em atracoes e pontos_turisticos

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
 
@@ -7,5 +9,33 @@ class PontoTuristicoViewSet(ModelViewSet):
     """
     A simple ViewSet for viewing and editing
     """
-    queryset = PontoTuristico.objects.all()
     serializer_class = PontoTuristicoSerializer
+
+    def get_queryset(self):
+    	return PontoTuristico.objects.filter(aprovado=True)
+
+    def list(self, request, *args, **kwargs):
+    	return super(PontoTuristicoViewSet, self).list(request, *args, **kwargs)
+
+    def create(self, request, *args, **kwargs):
+    	return super(PontoTuristicoViewSet, self).create(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+    	return super(PontoTuristicoViewSet, self).destroy(request, *args, **kwargs)
+
+    def retrieve(self, request, *args, **kwargs):
+    	return super(PontoTuristicoViewSet, self).retrieve(request, *args, **kwargs)
+
+    def update(self, request, *args, **kwargs):
+    	return super(PontoTuristicoViewSet, self).update(request, *args, **kwargs)
+
+    def partial_update(self, request, *args, **kwargs):
+    	return super(PontoTuristicoViewSet, self).partial_update(request, *args, **kwargs)
+
+    @action(methods=['get'], detail=True)
+    def denunciar(self, request, pk=None):
+    	pass
+
+    @action(methods=['get'], detail=False)
+    def teste(self, request):
+    	pass
