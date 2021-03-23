9e07e9e35543bc03a11346e5de4838f000de2e7a
Luciano Celinski <luciano@gruposc.com.br>
2021-03-23 18:43:04 -0300

    Implementa filtro na app atracoes

--- atracoes/api/viewsets.py
+++ atracoes/api/viewsets.py
@@ -3,6 +3,8 @@ from atracoes.models import Atracao
 from .serializers import AtracaoSerializer
 
 
+
 class AtracaoViewSet(ModelViewSet):
     queryset = Atracao.objects.all()
     serializer_class = AtracaoSerializer
+    filter_fields = ('nome', 'descricao')
