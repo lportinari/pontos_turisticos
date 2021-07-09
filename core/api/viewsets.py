d475388898d07bd5a9eecc24c258add4bf0e6fe0
Luciano Celinski <lvp.celinski@gmail.com>
2021-07-08 21:52:54 -0300

    Adiciona autenticação

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,4 +1,7 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework import authentication
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.filters import SearchFilter
@@ -7,59 +10,61 @@ from .serializers import PontoTuristicoSerializer
 
 
 class PontoTuristicoViewSet(ModelViewSet):
-    """
-    A simple ViewSet for viewing and editing
-    """
-    serializer_class = PontoTuristicoSerializer
-    filter_backends = (SearchFilter,)
-    search_fields = ('nome', 'descricao', 'endereco__linha1')
-    
-    # Altera o comportamento padrão de busca da api. Ao invés de
-    # buscar pelo ID, busca pelo campo informado.
-    #lookup_field = 'nome'
+	"""
+	A simple ViewSet for viewing and editing
+	"""
+	serializer_class = PontoTuristicoSerializer
+	filter_backends = (SearchFilter,)
+	permission_classes = (IsAuthenticated,)
+	authentication_classes = (TokenAuthentication,)
+	search_fields = ('nome', 'descricao', 'endereco__linha1')
+	
+	# Altera o comportamento padrão de busca da api. Ao invés de
+	# buscar pelo ID, busca pelo campo informado.
+	#lookup_field = 'nome'
 
-    def get_queryset(self):
-    	# Parâmetros passado na URL
-    	id = self.request.query_params.get('id', None)
-    	nome = self.request.query_params.get('nome', None)
-    	descricao = self.request.query_params.get('descricao', None)
+	def get_queryset(self):
+		# Parâmetros passado na URL
+		id = self.request.query_params.get('id', None)
+		nome = self.request.query_params.get('nome', None)
+		descricao = self.request.query_params.get('descricao', None)
 
-    	queryset = PontoTuristico.objects.all()
+		queryset = PontoTuristico.objects.all()
 
-    	if id:
-    		queryset = PontoTuristico.objects.filter(id=id)
+		if id:
+			queryset = PontoTuristico.objects.filter(id=id)
 
-    	if nome:
-    		# _iexact torna o filtro case insensitive
-    		queryset = queryset.filter(nome_iexact=nome)
+		if nome:
+			# _iexact torna o filtro case insensitive
+			queryset = queryset.filter(nome_iexact=nome)
 
-    	if descricao:
-    		queryset = queryset.filter(descricao_iexact=descricao)
+		if descricao:
+			queryset = queryset.filter(descricao_iexact=descricao)
 
-    	return queryset
+		return queryset
 
-    def list(self, request, *args, **kwargs):
-    	return super(PontoTuristicoViewSet, self).list(request, *args, **kwargs)
+	def list(self, request, *args, **kwargs):
+		return super(PontoTuristicoViewSet, self).list(request, *args, **kwargs)
 
-    def create(self, request, *args, **kwargs):
-    	return super(PontoTuristicoViewSet, self).create(request, *args, **kwargs)
+	def create(self, request, *args, **kwargs):
+		return super(PontoTuristicoViewSet, self).create(request, *args, **kwargs)
 
-    def destroy(self, request, *args, **kwargs):
-    	return super(PontoTuristicoViewSet, self).destroy(request, *args, **kwargs)
+	def destroy(self, request, *args, **kwargs):
+		return super(PontoTuristicoViewSet, self).destroy(request, *args, **kwargs)
 
-    def retrieve(self, request, *args, **kwargs):
-    	return super(PontoTuristicoViewSet, self).retrieve(request, *args, **kwargs)
+	def retrieve(self, request, *args, **kwargs):
+		return super(PontoTuristicoViewSet, self).retrieve(request, *args, **kwargs)
 
-    def update(self, request, *args, **kwargs):
-    	return super(PontoTuristicoViewSet, self).update(request, *args, **kwargs)
+	def update(self, request, *args, **kwargs):
+		return super(PontoTuristicoViewSet, self).update(request, *args, **kwargs)
 
-    def partial_update(self, request, *args, **kwargs):
-    	return super(PontoTuristicoViewSet, self).partial_update(request, *args, **kwargs)
+	def partial_update(self, request, *args, **kwargs):
+		return super(PontoTuristicoViewSet, self).partial_update(request, *args, **kwargs)
 
-    @action(methods=['get'], detail=True)
-    def denunciar(self, request, pk=None):
-    	pass
+	@action(methods=['get'], detail=True)
+	def denunciar(self, request, pk=None):
+		pass
 
-    @action(methods=['get'], detail=False)
-    def teste(self, request):
-    	pass
+	@action(methods=['get'], detail=False)
+	def teste(self, request):
+		pass
